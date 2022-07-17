2a7261f535f4faad2d08f797530b1e1fb7f54f19
NaimCaio <beckinhacoutinho@gmail.com>
2022-07-17 17:21:18 -0300

    projeto homepage webpack

---
+++ README.md
@@ -0,0 +1,19 @@
+# About homepage
+
+## Descrição
+- Single page aplication de um perfil inspirado em personagem de desenho para um estilo mais elaborado.
+
+## Instalação
+
+- Baixar ou clonar este repositório
+- Acessar com o terminal a pasta do projeto, baixado e **executar o comando**:
+```
+npm install 
+```
+## Execução
+- Na raiz do projeto, executar o comando:
+```
+npm run build
+
+npm run dev
+```
---
+++ src/js/index.js
@@ -0,0 +1,72 @@
+import '../scss/styles.scss'
+import imagePower01 from '../assets/images/pw01.jpg'
+import imagePower02 from '../assets/images/pw02.jpg'
+import imagePower03 from '../assets/images/pw03.jpg'
+import imagePower04 from '../assets/images/pw04.jpg'
+import imagePower05 from '../assets/images/pw05.jpg'
+import imagePower06 from '../assets/images/pw06.jpg'
+import imagePower07 from '../assets/images/pw07.jpg'
+import imagePower08 from '../assets/images/pw08.jpg'
+import imagePower09 from '../assets/images/pw09.jpg'
+
+const elemRoot = document.getElementById('root')
+elemRoot.classList.add('container')
+
+
+const elemImagePower01 = new Image()
+elemImagePower01.src = imagePower01
+
+const elemImagePower02 = new Image()
+elemImagePower02.src = imagePower02
+
+const elemImagePower03 = new Image()
+elemImagePower03.src = imagePower03
+
+const elemImagePower04 = new Image()
+elemImagePower04.src = imagePower04
+
+const elemImagePower05 = new Image()
+elemImagePower05.src = imagePower05
+
+const elemImagePower06 = new Image()
+elemImagePower06.src = imagePower06
+
+const elemImagePower07 = new Image()
+elemImagePower07.src = imagePower07
+
+const elemImagePower08 = new Image()
+elemImagePower08.src = imagePower08
+
+const elemImagePower09 = new Image()
+elemImagePower09.src = imagePower09
+
+
+
+document.querySelector('#power01')
+    .appendChild(elemImagePower01)
+
+document.querySelector('#power02')
+    .appendChild(elemImagePower02)
+
+document.querySelector('#power03')
+    .appendChild(elemImagePower03)
+
+document.querySelector('#power04')
+    .appendChild(elemImagePower04)
+
+document.querySelector('#power05')
+    .appendChild(elemImagePower05)
+
+document.querySelector('#power06')
+    .appendChild(elemImagePower06)
+
+document.querySelector('#power07')
+    .appendChild(elemImagePower07)
+
+document.querySelector('#power08')
+    .appendChild(elemImagePower08)
+
+
+
+
+
